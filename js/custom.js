8205d682fa32e684f31b1860c045aeeb013a7b8c
Luis Rovirosa <luisrovirosa@gmail.com>
2017-06-01 18:43:17 +0200

    contact form using ajax with response messages

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,21 @@
 /* Write here your custom javascript codes */
-function setContactAction($actionMessage){
+'use strict';
+
+function setContactAction($actionMessage) {
   $('#contact-action').val($actionMessage);
 }
+
+$('#formulario-contacto-form').submit(function(e) {
+  var url = '/php/demo-contacts-process.php';
+  $.post(url, $('#formulario-contacto-form').serialize())
+  .done(function() {
+    $('#formulario-contacto').fadeOut().promise().done(function() {
+      $('#gracias-contacto').fadeIn();
+    });
+  })
+  .fail(function(){
+    $('#error-contacto').fadeIn();
+    $("html, body").animate({ scrollTop: $(document).height() }, 1000);
+  });
+  e.preventDefault();
+});
