bfb5c5f43c39779ac75d81a03bb22de68926ebad
Luis Rovirosa <luisrovirosa@gmail.com>
2017-06-15 12:57:56 +0200

    fixed contact by email

--- php/demo-contacts-process.php
+++ php/demo-contacts-process.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 $transport = (new Swift_SmtpTransport('in-v3.mailjet.com', 587))
     ->setUsername('494fb687ee5235f531bc7a6a426bb6c1')
