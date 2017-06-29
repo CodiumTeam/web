caeb32efe0dba53392c3d57a6ee9945d8e7a8ad1
Jordi Anguela <jordi.anguela@gmail.com>
2017-06-29 12:49:09 +0200

    extract GA fn to method

--- js/custom.js
+++ js/custom.js
@@ -1,23 +1,27 @@
 /* Write here your custom javascript codes */
 'use strict';
 
+function trackEvent(category, action, label) {
+    ga('send', 'event', category, action, label);
+}
+
 function setContactAction(actionMessage) {
     $('#contact-action').val(actionMessage);
-    ga('send', 'event', 'contact_us', 'show_form', actionMessage);
+    trackEvent('contact_us', 'show_form', actionMessage);
 }
 
 $('#formulario-contacto-form').submit(function (e) {
     var url = '/php/demo-contacts-process.php';
     $.post(url, $('#formulario-contacto-form').serialize())
         .done(function () {
-            ga('send', 'event', 'contact_us', 'sent', $('#contact-action').val());
+            trackEvent('contact_us', 'sent', $('#contact-action').val());
 
             $('#formulario-contacto').fadeOut().promise().done(function () {
                 $('#gracias-contacto').fadeIn();
             });
         })
         .fail(function () {
-            ga('send', 'event', 'contact_us', 'failed');
+            trackEvent('contact_us', 'failed');
 
             $('#error-contacto').fadeIn();
             $("html, body").animate({scrollTop: $(document).height()}, 1000);
@@ -26,11 +30,11 @@ $('#formulario-contacto-form').submit(function (e) {
 });
 
 $('#training-more-info-btn').click(function () {
-    ga('send', 'event', 'services', 'more_information', 'training');
+    trackEvent('services', 'more_information', 'training');
 });
 $('#coaching-more-info-btn').click(function () {
-    ga('send', 'event', 'services', 'more_information', 'coaching');
+    trackEvent('services', 'more_information', 'coaching');
 });
 $('#development-more-info-btn').click(function () {
-    ga('send', 'event', 'services', 'more_information', 'development');
+    trackEvent('services', 'more_information', 'development');
 });
