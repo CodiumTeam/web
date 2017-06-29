3f3ab7cfc62fc5bfeb68b7021a1ff8f5279168f6
Jordi Anguela <jordi.anguela@gmail.com>
2017-06-29 12:06:46 +0200

    add GA event to track Contact Us goal + reformat

--- js/custom.js
+++ js/custom.js
@@ -2,20 +2,24 @@
 'use strict';
 
 function setContactAction($actionMessage) {
-  $('#contact-action').val($actionMessage);
+    $('#contact-action').val($actionMessage);
 }
 
-$('#formulario-contacto-form').submit(function(e) {
-  var url = '/php/demo-contacts-process.php';
-  $.post(url, $('#formulario-contacto-form').serialize())
-  .done(function() {
-    $('#formulario-contacto').fadeOut().promise().done(function() {
-      $('#gracias-contacto').fadeIn();
-    });
-  })
-  .fail(function(){
-    $('#error-contacto').fadeIn();
-    $("html, body").animate({ scrollTop: $(document).height() }, 1000);
-  });
-  e.preventDefault();
+$('#formulario-contacto-form').submit(function (e) {
+    var url = '/php/demo-contacts-process.php';
+    $.post(url, $('#formulario-contacto-form').serialize())
+        .done(function () {
+            ga('send', 'event', 'contact_us', 'sent', $('#contact-action').val());
+
+            $('#formulario-contacto').fadeOut().promise().done(function () {
+                $('#gracias-contacto').fadeIn();
+            });
+        })
+        .fail(function () {
+            ga('send', 'event', 'contact_us', 'failed');
+
+            $('#error-contacto').fadeIn();
+            $("html, body").animate({scrollTop: $(document).height()}, 1000);
+        });
+    e.preventDefault();
 });
