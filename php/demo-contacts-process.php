12b93d93fb223abc1f13f321e8ff35c103a4486d
Luis Rovirosa <luisrovirosa@gmail.com>
2017-07-31 09:56:11 +0200

    Main: contact form validating recaptcha on backend

--- php/demo-contacts-process.php
+++ php/demo-contacts-process.php
@@ -1,27 +1,43 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$transport = (new Swift_SmtpTransport('in-v3.mailjet.com', 587))
-    ->setUsername('494fb687ee5235f531bc7a6a426bb6c1')
-    ->setPassword('9dd332b69575a9ab2a49cc65a7ee6b20');
+validateRecaptcha();
+sendEmail();
 
-$mailer = new Swift_Mailer($transport);
+function validateRecaptcha()
+{
+    $recaptcha = $_POST['g-recaptcha-response'];
+    $client = new \GuzzleHttp\Client();
+    $response = $client->request('POST', 'https://www.google.com/recaptcha/api/siteverify', [
+        'form_params' => [
+            'secret'   => '6LcIGCsUAAAAAGpVhL28cuEsHVqRipF8r7XmPQOz',
+            'response' => $recaptcha
+        ]
+    ]);
 
-$email = $_POST['email'];
-$name = $_POST['name'];
-$body = $_POST['message'] . "\n\nEmail $email";
-$subject = '[Dojo] ' . $_POST['action'];
+    $responseJson = json_decode($response->getBody()->getContents(), true);
+    if (!$responseJson['success']) {
+        throw new Exception("El captcha no es válido", 400);
+    }
+}
 
-$message = (new Swift_Message($subject))
-    ->setFrom(['luisrovirosa@gmail.com' => $name])
-    ->setTo(['luisrovirosa@gmail.com' => 'Luis Rovirosa', 'jordi.anguela@gmail.com' => 'Jordi Anguela'])
-    ->setBody($body);
+function sendEmail()
+{
+    $transport = (new Swift_SmtpTransport('in-v3.mailjet.com', 587))
+        ->setUsername('494fb687ee5235f531bc7a6a426bb6c1')
+        ->setPassword('9dd332b69575a9ab2a49cc65a7ee6b20');
 
-$result = $mailer->send($message);
+    $mailer = new Swift_Mailer($transport);
 
+    $email = $_POST['email'];
+    $name = $_POST['name'];
+    $body = $_POST['message'] . "\n\nEmail $email";
+    $subject = '[Dojo] ' . $_POST['action'];
 
+    $message = (new Swift_Message($subject))
+        ->setFrom(['luisrovirosa@gmail.com' => $name])
+        ->setTo(['luisrovirosa@gmail.com' => 'Luis Rovirosa', 'jordi.anguela@gmail.com' => 'Jordi Anguela'])
+        ->setBody($body);
 
-
-
-
-
+    $mailer->send($message);
+}
