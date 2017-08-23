5ea83c3b7c02db737f6ee4a04a0aeb62fe834605
Jordi Anguela <jordi.anguela@gmail.com>
2017-08-23 18:03:09 +0200

    tdd landing mobile responsiveness

--- js/tdd.js
+++ js/tdd.js
@@ -1,16 +1,30 @@
 var form = $('#tdd-contact-form');
 form.submit(function (e) {
-  var url = '/php/contact-tdd.php';
-  $.post(url, form.serialize())
-  .done(function () {
-    // $('#error-contacto').fadeOut();
-    $('#tdd-contact').fadeOut().promise().done(function () {
-      $('#gracias-contacto-tdd').fadeIn();
-    });
-  })
-  .fail(function () {
-    // $('#error-contacto').fadeIn();
-    // $("html, body").animate({scrollTop: $(document).height()}, 1000);
-  });
-  e.preventDefault();
+    var url = '/php/contact-tdd.php';
+    $.post(url, form.serialize())
+        .done(function () {
+            // $('#error-contacto').fadeOut();
+            $('#tdd-contact').fadeOut().promise().done(function () {
+                $('#gracias-contacto-tdd').fadeIn();
+            });
+        })
+        .fail(function () {
+            // $('#error-contacto').fadeIn();
+            // $("html, body").animate({scrollTop: $(document).height()}, 1000);
+        });
+    e.preventDefault();
+});
+
+function fixLandingResponsiveness() {
+    var browserWidth = $(window).width();
+    var desktopDeviceWidth = 992;
+    if (browserWidth < desktopDeviceWidth) {
+        $('#landing').removeClass('fullheight');
+    } else {
+        $('#landing').addClass('fullheight');
+    }
+}
+
+$(window).resize(function() {
+    fixLandingResponsiveness();
 });
