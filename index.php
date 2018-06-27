934da899fd6e864bca6dca15c4f8ec20fc48bdba
Luis Rovirosa <luisrovirosa@gmail.com>
2018-06-27 13:19:34 +0200

    service as a component

--- index.php
+++ index.php
@@ -6,4 +6,6 @@ $loader = new Twig_Loader_Filesystem('templates');
 $twig = new Twig_Environment($loader, [
 ]);
 
-echo $twig->render('index.html.twig', ['name' => 'Fabien']);
+$data = $twig->render('index.html.twig');
+echo $data;
+file_put_contents('twig.html', $data);
