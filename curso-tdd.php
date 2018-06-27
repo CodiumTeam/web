79d4797c881342d8a0e858f3cc40a164f83b5557
Luis Rovirosa <luisrovirosa@gmail.com>
2018-06-27 13:57:24 +0200

    starts tdd as twig

--- curso-tdd.php
+++ curso-tdd.php
@@ -6,6 +6,6 @@ $loader = new Twig_Loader_Filesystem('templates');
 $twig = new Twig_Environment($loader, [
 ]);
 
-$data = $twig->render('index.html.twig');
+$data = $twig->render('tdd.html.twig');
 echo $data;
-file_put_contents('twig-index.html', $data);
+file_put_contents('twig-tdd.html', $data);
