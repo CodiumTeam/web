e2e40fdc4d97695c60b9df629f1fe536221c9762
Luis Rovirosa <luisrovirosa@gmail.com>
2018-06-29 12:46:18 +0200

    adds legacy send email

---
+++ js/legacy.js
@@ -0,0 +1,33 @@
+var form = $('#tdd-contact-form');
+form.submit(function (e) {
+    var url = '/php/contact-legacy-code.php';
+    $.post(url, form.serialize())
+        .done(function () {
+        trackEvent('legacy_training', 'sent');
+
+        // $('#error-contacto').fadeOut();
+            $('#tdd-contact').fadeOut().promise().done(function () {
+                $('#gracias-contacto-tdd').fadeIn();
+            });
+        })
+        .fail(function () {
+            // $('#error-contacto').fadeIn();
+            // $("html, body").animate({scrollTop: $(document).height()}, 1000);
+        });
+    e.preventDefault();
+});
+
+function fixLandingResponsiveness() {
+    var browserWidth = $(window).width();
+    var desktopDeviceWidth = 992;
+    if (browserWidth < desktopDeviceWidth) {
+        $('#landing').removeClass('fullheight');
+        $('#landing').css("height", "inherit");
+    } else {
+        $('#landing').addClass('fullheight');
+    }
+}
+
+$(window).resize(function() {
+    fixLandingResponsiveness();
+});
---
+++ php/contact-legacy-code.php
@@ -0,0 +1,9 @@
+<?php
+require_once __DIR__ . '/../vendor/autoload.php';
+
+//validateRecaptcha($_POST['g-recaptcha-response']);
+$params = $_POST;
+$params['subject'] = 'Curso de Legacy Code ' . date("Y-m-d H:i:s");
+sendEmail($params);
+
+
