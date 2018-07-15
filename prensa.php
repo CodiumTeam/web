28a406037e391ee2d3c15dbbe32a5373327936c7
Jordi Anguela <jordi.anguela@gmail.com>
2018-07-15 20:45:18 +0200

    refactor rename template

--- prensa.php
+++ prensa.php
@@ -6,6 +6,6 @@ $loader = new Twig_Loader_Filesystem('templates');
 $twig = new Twig_Environment($loader, [
 ]);
 
-$data = $twig->render('prensa.html.twig');
+$data = $twig->render('press.html.twig');
 echo $data;
 file_put_contents('prensa.html', $data);
