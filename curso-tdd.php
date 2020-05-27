262b06f905c5edd9e4fafc127010116f690003c0
Luis Rovirosa Mairlot <luis@codium.team>
2020-05-27 08:51:19 +0200

    updates version of twig and php

--- curso-tdd.php
+++ curso-tdd.php
@@ -1,10 +1,12 @@
 <?php
+
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
+
 require_once 'vendor/autoload.php';
 
-$loader = new Twig_Loader_Filesystem('templates');
-
-$twig = new Twig_Environment($loader, [
-]);
+$loader = new FilesystemLoader('templates');
+$twig = new Environment($loader, []);
 
 $data = preg_replace('/\s+/', ' ', $twig->render('tdd.html.twig'));
 echo $data;
--- index.php
+++ index.php
@@ -1,9 +1,12 @@
 <?php
+
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
+
 require_once 'vendor/autoload.php';
 
-$loader = new Twig_Loader_Filesystem('templates');
-
-$twig = new Twig_Environment($loader, []);
+$loader = new FilesystemLoader('templates');
+$twig = new Environment($loader, []);
 
 $data = preg_replace('/\s+/', ' ', $twig->render('index.html.twig'));
 echo $data;
