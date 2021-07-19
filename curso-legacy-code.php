47a4a8b3a9edd0ce704afb3f5a9fca73eefa4ba6
Jordi Anguela <jordi@codium.team>
2021-07-19 10:41:58 +0200

    limit the number of old trainings displayed in mobile devices
    Co-authored-by: Concha Asensio <concha@codium.team>

--- curso-legacy-code.php
+++ curso-legacy-code.php
@@ -7,7 +7,8 @@ require_once 'vendor/autoload.php';
 
 $loader = new FilesystemLoader('templates');
 $twig = new Environment($loader, []);
+$detect = new Mobile_Detect;
 
-$data = preg_replace('/\s+/', ' ', $twig->render('legacy.html.twig'));
+$data = preg_replace('/\s+/', ' ', $twig->render('legacy.html.twig', ['isMobile' => $detect->isMobile()]));
 echo $data;
 file_put_contents('curso-legacy-code.html', $data);
