a53ed773a771b8a4a2e1f93887827a06611bee82
Jose Cabrera <jose@codium.team>
2022-03-09 16:14:54 +0100

    Handle focus only when there are no value

--- js/course.js
+++ js/course.js
@@ -14,16 +14,30 @@ glide.mount();
 document
   .querySelectorAll('input[type=radio][name="myRadio"]')
   .forEach(function (radio) {
-    radio.addEventListener('change', function (event) {
-      const { value } = event.target;
-
-      document.getElementById('js-locality').classList.toggle('hidden');
-      document.getElementById('js-numProgrammers').classList.toggle('hidden');
-
-      if (value === 'business') {
-        document.getElementById('numProgrammers').focus();
-      } else {
-        document.getElementById('locality').focus();
-      }
-    });
+    radio.addEventListener('change', showCorrectInputDependingOnSelectedRadio);
   });
+
+function showCorrectInputDependingOnSelectedRadio(event) {
+  const { value } = event.target;
+
+  document.getElementById('js-locality').classList.toggle('hidden');
+  document.getElementById('js-numProgrammers').classList.toggle('hidden');
+
+  if (value === 'business') {
+    const $numProgrammers = document.getElementById('numProgrammers');
+
+    if (!hasValue($numProgrammers)) {
+      $numProgrammers.focus();
+    }
+  } else {
+    const $localityInput = document.getElementById('locality');
+
+    if (!hasValue($localityInput)) {
+      $localityInput.focus();
+    }
+  }
+}
+
+function hasValue(inputElement) {
+  return inputElement.value.length > 0;
+}
