6c2ff6b6dab6afb6511f91ea12719f7e85e30383
Jose Cabrera <jose@codium.team>
2022-03-09 16:10:21 +0100

    Show different input when use change from business to 'for m' radio

--- js/course.js
+++ js/course.js
@@ -10,3 +10,20 @@ var glide = new Glide('.glide', {
 });
 
 glide.mount();
+
+document
+  .querySelectorAll('input[type=radio][name="myRadio"]')
+  .forEach(function (radio) {
+    radio.addEventListener('change', function (event) {
+      const { value } = event.target;
+
+      document.getElementById('js-locality').classList.toggle('hidden');
+      document.getElementById('js-numProgrammers').classList.toggle('hidden');
+
+      if (value === 'business') {
+        document.getElementById('numProgrammers').focus();
+      } else {
+        document.getElementById('locality').focus();
+      }
+    });
+  });
