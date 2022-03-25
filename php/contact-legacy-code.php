ce756a487a1e7606e6201cfd65bdaee1d300e192
Luis Rovirosa Mairlot <luis@codium.team>
2022-03-25 09:29:09 +0100

    add validation to contact forms

--- php/contact-legacy-code.php
+++ php/contact-legacy-code.php
@@ -1,6 +1,10 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
 
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    return http_response_code(405);
+}
+
 validateRecaptcha($_POST['g-recaptcha-response']);
 $params = $_POST;
 $params['subject'] = 'Curso de Legacy Code ' . date("Y-m-d H:i:s");
