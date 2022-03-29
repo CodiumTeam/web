85ca44ccdbc81421e3a2c8191c21df6d546765cc
Jose Cabrera <jose@codium.team>
2022-03-29 13:21:05 +0200

    Introduce nunjucks

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,22 @@
 import { defineConfig } from 'vite';
-import handlebars from 'vite-plugin-handlebars';
+import nunjucks from 'vite-plugin-nunjucks';
+import fs from 'fs';
+import { resolve } from 'path';
+
+const ROOT = resolve(__dirname);
+const pages = fs.readdirSync(resolve(ROOT, 'pages'));
+
+const inputs = {};
+
+pages.forEach((page) => {
+  inputs[page] = resolve(ROOT, 'pages', page);
+});
 
 export default defineConfig({
+  plugins: [nunjucks()],
   build: {
     rollupOptions: {
-      input: {
-        'index.html': './index.ejs',
-        'home.html': './home.html',
-      },
+      input: inputs,
     },
   },
 });
