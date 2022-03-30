7d3a3ea922d64e76e4cefa783bc299955c6c52d0
Jose Cabrera <jose@codium.team>
2022-03-30 18:30:10 +0200

    Substract padding to element

--- src/js/reveal.js
+++ src/js/reveal.js
@@ -3,10 +3,9 @@ export function reveal() {
 
   for (var i = 0; i < reveals.length; i++) {
     var windowHeight = window.innerHeight;
-    var elementTop = reveals[i].getBoundingClientRect().top;
-    var elementVisible = 150;
+    var elementTop = reveals[i].getBoundingClientRect().top - 30;
 
-    if (elementTop < windowHeight - elementVisible) {
+    if (elementTop < windowHeight) {
       reveals[i].classList.add('active');
     }
   }
