a777035f0c32cf29c555f50deddc5d435e79255f
Jose Cabrera <jose@codium.team>
2022-04-13 17:44:18 +0200

    Change id and header position

--- src/js/common/scrollToElement.js
+++ src/js/common/scrollToElement.js
@@ -1,10 +1,6 @@
 export const scrollToElement = (element) => {
   const yOffset = document.getElementById('js-header').offsetHeight;
-  const blockPadding = 32;
-  const y =
-    element.getBoundingClientRect().top +
-    window.pageYOffset -
-    (yOffset + blockPadding);
+  const y = element.getBoundingClientRect().top + window.pageYOffset - yOffset;
 
   window.scrollTo({ top: y, behavior: 'smooth' });
 };
