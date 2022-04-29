e458584f0862245f724b624d9f3ade68c7db8025
Jose Cabrera <jose@codium.team>
2022-04-29 13:33:23 +0200

    refactor bootcamp menu tests

---
+++ cypress/integration/pages/landings/menu-bootcamp.spec.js
@@ -0,0 +1,61 @@
+/// <reference types="cypress" />
+
+describe('User navigation', () => {
+  const devices = ['desktop', 'iphone-6'];
+
+  devices.forEach((device) => {
+    describe(`From navbar emulating ${device}`, () => {
+      beforeEach(() => {
+        if (isMobile(device)) {
+          cy.viewport('iphone-6');
+        }
+
+        cy.visit('/programa-de-aceleracion.html');
+
+        if (isMobile(device)) {
+          cy.get('#js-menu-button').click();
+        }
+      });
+
+      it('Should navigate to course benefits block in bootcamp page', () => {
+        cy.get('.navbar__item').should('contain', 'Beneficios');
+        cy.get('.navbar__item').first().click();
+        cy.isInViewport('#benefits');
+      });
+
+      it('Should navigate to the course content block in bootcamp page', () => {
+        cy.get('.navbar__item').eq(1).should('contain', 'Contenido');
+        cy.get('.navbar__item').eq(1).click();
+        cy.isInViewport('#content');
+      });
+
+      it('Should navigate to the course methodology block in bootcamp page', () => {
+        cy.get('.navbar__item').eq(2).should('contain', 'Metodología');
+        cy.get('.navbar__item').eq(2).click();
+        cy.isInViewport('#methodology');
+      });
+
+      it('Should navigate to the course faqs block in bootcamp page', () => {
+        cy.get('.navbar__item').eq(3).should('contain', 'Equipo');
+        cy.get('.navbar__item').eq(3).click();
+        cy.isInViewport('#team');
+      });
+
+      it('Should navigate to the course faqs block in bootcamp page', () => {
+        cy.get('.navbar__item').eq(4).should('contain', 'FAQs');
+        cy.get('.navbar__item').eq(4).click();
+        cy.isInViewport('#faqs');
+      });
+
+      it('Should navigate to the course contact block in bootcamp page', () => {
+        cy.get('.navbar__item').eq(5).should('contain', 'Contactar');
+        cy.get('.navbar__item').eq(5).click();
+        cy.isInViewport('#contact');
+      });
+    });
+  });
+});
+
+function isMobile(device) {
+  return device !== 'desktop';
+}
