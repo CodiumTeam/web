91b478d86225a15cf0ac3531ca03f13804a4f297
Jose Cabrera <jose@codium.team>
2022-04-29 11:18:11 +0200

    Add carousel in development

---
+++ src/js/common/contact-form-with-message.js
@@ -0,0 +1,105 @@
+import * as formValidation from './fromValidation';
+import { scrollToElement } from './scrollToElement';
+
+handleFormSubmit();
+
+function validateForm() {
+  const $name = document.getElementById('name');
+  const $email = document.getElementById('email');
+  const $message = document.getElementById('message');
+
+  const hasValidName = formValidation.validateInput($name);
+  const hasValidEmail = formValidation.validateInput($email);
+  const hasValidMessage = formValidation.validateInput($message);
+
+  if (!hasValidName || !hasValidEmail || !hasValidMessage) {
+    if (!hasValidName) {
+      $name.focus();
+      return false;
+    }
+
+    if (!hasValidEmail) {
+      $email.focus();
+      return false;
+    }
+
+    if (!hasValidMessage) {
+      $message.focus();
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function handleFormSubmit() {
+  document
+    .getElementById('contactForm')
+    .addEventListener('submit', function (event) {
+      event.preventDefault();
+
+      const isValid = validateForm();
+
+      if (!isValid) return;
+
+      if (!grecaptcha.getResponse()) {
+        grecaptcha.execute();
+        return;
+      }
+    });
+}
+
+// This is called via data-callback
+window.captchaCompleted = () => {
+  const isValid = validateForm();
+
+  if (!isValid) {
+    grecaptcha.reset();
+    return;
+  }
+
+  const $form = document.getElementById('contactForm');
+  const formData = new FormData($form);
+  const trainingType = $form.getAttribute('data-training-type');
+  formData.append('trainingType', trainingType);
+
+  const $errorBlock = document.getElementById('js-show-error');
+
+  document.getElementById('js-submit').disabled = true;
+
+  fetch('/php/contact.php', {
+    method: 'POST',
+    body: formData,
+  })
+    .then(function (response) {
+      grecaptcha.reset();
+      if (response.ok) {
+        events.trackEvent('contact_us', 'sent', 'home');
+        $form.remove();
+
+        const $successBlock = document.getElementById('js-show-success');
+        $successBlock.classList.remove('hidden');
+
+        const yOffset = document.getElementById('js-header').offsetHeight;
+        const y =
+          $successBlock.getBoundingClientRect().top +
+          window.pageYOffset -
+          yOffset;
+
+        $errorBlock.remove();
+        window.scrollTo({ top: y, behavior: 'smooth' });
+      } else {
+        $errorBlock.classList.remove('hidden');
+        scrollToElement($errorBlock);
+        events.trackEvent('contact_us', 'failed', 'home');
+        document.getElementById('js-submit').disabled = false;
+      }
+    })
+    .catch(() => {
+      grecaptcha.reset();
+      $errorBlock.classList.remove('hidden');
+      scrollToElement($errorBlock);
+      events.trackEvent('contact_us', 'failed', 'home');
+      document.getElementById('js-submit').disabled = false;
+    });
+};
--- src/js/development.js
+++ src/js/development.js
@@ -1,2 +1,50 @@
-import './site';
+import Glide, {
+  Controls,
+  Breakpoints,
+} from '@glidejs/glide/dist/glide.modular.esm';
+import './common/contact-form-with-message';
+
+import '@glidejs/glide/dist/css/glide.core.min.css';
+import '@glidejs/glide/dist/css/glide.theme.min.css';
 import '../sass/development.scss';
+
+mountClientsCarousel();
+
+function mountClientsCarousel() {
+  const glide = new Glide('#clients', {
+    type: 'carousel',
+    autoplay: false,
+    startAt: 0,
+    gap: 10,
+    perView: 5,
+    breakpoints: {
+      450: {
+        perView: 1,
+      },
+      600: {
+        perView: 2,
+      },
+      900: {
+        perView: 3,
+      },
+      980: {
+        perView: 4,
+      },
+    },
+  });
+
+  glide.mount({ Controls, Breakpoints });
+
+  addClientEventForArrows('.js-clients-arrow', glide);
+}
+
+// This is a fix for https://github.com/glidejs/glide/issues/417
+function addClientEventForArrows(arrowClass, glide) {
+  const glideArrows = document.querySelectorAll(arrowClass);
+
+  glideArrows.forEach(function (glideArrow) {
+    glideArrow.addEventListener('click', function () {
+      glide.go(glideArrow.dataset.glideDir);
+    });
+  });
+}
