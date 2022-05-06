6c4dc3806862801292723c35f46ba51f256e7a4b
Jose Cabrera <jose@codium.team>
2022-05-06 13:06:47 +0200

    Fix styles

--- src/js/challenge-app/tdd-challenge/Challenge-01.jsx
+++ src/js/challenge-app/tdd-challenge/Challenge-01.jsx
@@ -1,44 +1,16 @@
-import React, { useEffect, useState } from 'react';
-import sdk from '@stackblitz/sdk';
+import React from 'react';
+import Editor from '../components/Editor';
 
 function Challenge01() {
-  const [error, setShowError] = useState(false);
-
-  useEffect(() => {
-    setShowError(true);
-
-    sdk
-      .embedProjectId('code', 'tdd-challenge-1', {
-        forceEmbedLayout: true,
-        openFile: 'README.md',
-        view: 'editor',
-        theme: 'dark',
-        hideNavigation: true,
-      })
-      .catch(() => {
-        setShowError(true);
-      });
-
-    return () => {};
-  }, []);
-
   return (
-    <div>
-      <div id="code" className="code"></div>
-      {error && (
-        <div className="alert alert--error hidden" id="js-show-error">
-          <div className="alert__content">
-            <i className="icon icon-warning"></i>
-            <p>Ha ocurrido un error, por favor inténtelo de nuevo.</p>
-          </div>
-          <p>
-            <small>
-              Si estás en modo incógnito. El editor no funcionará debido a
-              bloqueos de ciertas APIS por parte del navegador
-            </small>
-          </p>
-        </div>
-      )}
+    <div className="editor-wrapper">
+      <Editor
+        challengeId="tdd-challenge-1"
+        onLoad={(vm) => {
+          console.log(vm);
+        }}
+      />
+      <button className="button solution-btn">Mostrar solución</button>
     </div>
   );
 }
