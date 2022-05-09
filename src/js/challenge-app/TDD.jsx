4e7b7939a1e741504d9a5e309d080c3d3f451d91
Jose Cabrera <jose@codium.team>
2022-05-09 16:07:01 +0200

    add warning modal and hide button in lastStep

--- src/js/challenge-app/TDD.jsx
+++ src/js/challenge-app/TDD.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
+import Modal from 'react-modal';
 import { listenDropdown } from '../common/dropdown';
-import './challenge.scss';
-import './challenge.scss';
 import Stepper from './components/Stepper/Stepper';
 import WhatIsTDD from './tdd-challenge/03-WhatIsTdd';
 import Welcome from './tdd-challenge/01-Welcome';
@@ -10,35 +9,46 @@ import Precode from './tdd-challenge/04-PrecodeRememberSep';
 import Challenge from './tdd-challenge/05-Challenge';
 import Congratulations from './tdd-challenge/06-Congratulations';
 import { useStepper } from './components/Stepper/useStepper';
+import WarningImg from '../../img/challenges/warning.png';
+
+import './challenge.scss';
 
 listenDropdown();
 
 function TDD() {
   const codeStep = 4;
-  const { step, nextStep, prevStep } = useStepper(6);
+  const { step, nextStep, prevStep } = useStepper();
   const [stepperBtnText, setStepperBtnText] = useState('Siguiente');
   const [answers, setAnswers] = useState({
     whatIsTdd: null,
     tddCycle: null,
   });
   const [isDisable, setIsDisable] = useState(false);
+  const [showExitModal, setShowExitModal] = useState(false);
 
   function handleNextStep() {
+    if (step === codeStep) {
+      setShowExitModal(true);
+      return;
+    }
+
+    goToNext();
+  }
+
+  function goToNext() {
     const currStep = nextStep();
     const whatIsTDDStep = 1;
     const tddCycleStep = 2;
-    if (currStep === whatIsTDDStep && answers.whatIsTdd === null) {
-      setIsDisable(true);
-    }
+    setStepperBtnText('Siguiente');
 
-    if (currStep === tddCycleStep && answers.tddCycle === null) {
-      setIsDisable(true);
+    if ([whatIsTDDStep, tddCycleStep].includes(currStep)) {
+      if (answers.tddCycle === null || answers.tddCycle === null) {
+        setIsDisable(true);
+      }
     }
 
     if (currStep === codeStep) {
       setStepperBtnText('Finalizar');
-    } else {
-      setStepperBtnText('Siguiente');
     }
   }
 
@@ -60,44 +70,90 @@ function TDD() {
   }
 
   return (
-    <Stepper
-      step={step}
-      controls={
-        <Stepper.Controls
-          step={step}
-          onNextStepClick={handleNextStep}
-          onBackStepClick={handleBackStep}
-          nextButtonText={stepperBtnText}
-          nextButtonDisabled={isDisable}
-          hideBackButton={step === codeStep}
+    <>
+      <Stepper
+        step={step}
+        controls={
+          step >= codeStep + 1 ? null : (
+            <Stepper.Controls
+              step={step}
+              onNextStepClick={handleNextStep}
+              onBackStepClick={handleBackStep}
+              nextButtonText={stepperBtnText}
+              nextButtonDisabled={isDisable}
+              hideBackButton={step === codeStep}
+            />
+          )
+        }
+      >
+        <Stepper.Step>
+          <Welcome />
+        </Stepper.Step>
+        <Stepper.Step>
+          <WhatIsTDD
+            onSelect={handleWhatIsTddAnswer}
+            userAnswer={answers.whatIsTdd}
+          />
+        </Stepper.Step>
+        <Stepper.Step>
+          <TDDCycle
+            onSelect={handleTDDCicleAnswer()}
+            userAnswer={answers.tddCycle}
+          />
+        </Stepper.Step>
+        <Stepper.Step>
+          <Precode />
+        </Stepper.Step>
+        <Stepper.Step>
+          <Challenge />
+        </Stepper.Step>
+        <Stepper.Step>
+          <Congratulations />
+        </Stepper.Step>
+      </Stepper>
+      {showExitModal && (
+        <ModalExistWarning
+          modalIsOpen={showExitModal}
+          onExit={() => {
+            setShowExitModal(false);
+            goToNext();
+          }}
+          onCancel={() => {
+            setShowExitModal(false);
+          }}
         />
-      }
+      )}
+    </>
+  );
+}
+
+function ModalExistWarning({ modalIsOpen, onExit, onCancel }) {
+  return (
+    <Modal
+      isOpen={modalIsOpen}
+      className="Modal"
+      overlayClassName="Overlay"
+      onRequestClose={() => {}}
+      contentLabel="Example Modal"
     >
-      <Stepper.Step>
-        <Welcome />
-      </Stepper.Step>
-      <Stepper.Step>
-        <WhatIsTDD
-          onSelect={handleWhatIsTddAnswer}
-          userAnswer={answers.whatIsTdd}
-        />
-      </Stepper.Step>
-      <Stepper.Step>
-        <TDDCycle
-          onSelect={handleTDDCicleAnswer()}
-          userAnswer={answers.tddCycle}
-        />
-      </Stepper.Step>
-      <Stepper.Step>
-        <Precode />
-      </Stepper.Step>
-      <Stepper.Step>
-        <Challenge />
-      </Stepper.Step>
-      <Stepper.Step>
-        <Congratulations />
-      </Stepper.Step>
-    </Stepper>
+      <section className="card kata-explanation ">
+        <img src={WarningImg} />
+        <h1>¡Cuidado!</h1>
+        <p>
+          Al finalizar la kata podrías perder todos tus cambios.{' '}
+          <strong>¿Deseas continuar?</strong>
+        </p>
+
+        <div className="button__wrapper exit">
+          <button className="button button--outline" onClick={onCancel}>
+            Cancelar
+          </button>
+          <button className="button button--primary" onClick={onExit}>
+            Sí, finalizar kata
+          </button>
+        </div>
+      </section>
+    </Modal>
   );
 }
 
