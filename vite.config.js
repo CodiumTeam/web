6360bacf739271550c158bae3dd8d8c7cb69c949
Jose Cabrera <jose@codium.team>
2022-05-16 17:57:08 +0200

    fix legacy challenge page

--- vite.config.js
+++ vite.config.js
@@ -17,7 +17,7 @@ const inputs = {
   development: resolve(SRC, 'desarrollo.html'),
   404: resolve(SRC, '404.html'),
   'tdd-challenge': resolve(SRC, 'tdd-challenge.html'),
-  'legacy-challenge': resolve(SRC, 'tdd-challenge.html'),
+  'legacy-challenge': resolve(SRC, 'legacy-challenge.html'),
 };
 
 export default defineConfig({
