f29ff7f11322ccf464a161a7a1b23fdfaf25402d
Jose Cabrera <jose@codium.team>
2022-05-16 12:26:42 +0200

    [refactor]: legacy use hook

--- src/js/challenge-app/Legacy.jsx
+++ src/js/challenge-app/Legacy.jsx
@@ -1,58 +1,45 @@
 import React, { useState } from 'react';
 import { listenDropdown } from '../common/dropdown';
 import Stepper from './components/Stepper/Stepper';
-import { useStepper } from './components/Stepper/useStepper';
 import Welcome from './legacy-challenge/01-Welcome';
 import WhatIsLegacy from './legacy-challenge/02-WhatIsLegacy';
 import LegacyChallenge from './legacy-challenge/03-Challenge';
 import Congratulations from './legacy-challenge/04-Congratulations';
 import ModalExistWarning from './components/ModalExistWarning';
+import { useChallenge } from './useChallenge';
 
 listenDropdown();
 
 function Legacy() {
-  const codeStep = 2;
-  const { step, nextStep, prevStep } = useStepper();
-  const [stepperBtnText, setStepperBtnText] = useState('Siguiente');
-  const [isDisable, setIsDisable] = useState(false);
-  const [showExitModal, setShowExitModal] = useState(false);
+  const steps = {
+    whatIsLegacy: 1,
+    codeStep: 2,
+  };
   const [answers, setAnswers] = useState({
     whatIsLegacy: null,
   });
 
-  function handleNextStep() {
-    if (step === codeStep) {
-      setShowExitModal(true);
-      return;
-    }
-
-    goToNext();
-  }
-
-  function goToNext() {
-    const whatIsLegacyStep = 1;
-    const currStep = nextStep();
-    setStepperBtnText('Siguiente');
-
-    if (whatIsLegacyStep == currStep) {
-      if (answers.whatIsLegacy === null) {
-        setIsDisable(true);
-      }
-    }
-
-    if (currStep === codeStep) {
-      setStepperBtnText('Finalizar');
-    }
-  }
-
-  function handleBackStep() {
-    setIsDisable(false);
-    prevStep();
-  }
+  const {
+    step,
+    handleNextStep,
+    handleBackStep,
+    stepperBtnText,
+    isDisable,
+    showExitModal,
+    hideExitModal,
+    updateRequiredSteps,
+  } = useChallenge({
+    codeStep: steps.codeStep,
+    requiredSteps: [steps.whatIsLegacy],
+  });
 
   function handleWhatIsLegactAnswer(answerId) {
     setAnswers({ ...answers, whatIsLegacy: answerId });
-    setIsDisable(false);
+    updateRequiredSteps([]);
+  }
+
+  function isInLastStep() {
+    return step >= steps.codeStep + 1;
   }
 
   return (
@@ -60,14 +47,18 @@ function Legacy() {
       <Stepper
         step={step}
         controls={
-          step >= codeStep + 1 ? null : (
+          isInLastStep() ? null : (
             <Stepper.Controls
               step={step}
-              onNextStepClick={handleNextStep}
-              onBackStepClick={handleBackStep}
+              onNextStepClick={() => {
+                handleNextStep(false);
+              }}
+              onBackStepClick={() => {
+                handleBackStep(false);
+              }}
               nextButtonText={stepperBtnText}
               nextButtonDisabled={isDisable}
-              hideBackButton={step === codeStep}
+              hideBackButton={step === steps.codeStep}
             />
           )
         }
@@ -92,11 +83,11 @@ function Legacy() {
         <ModalExistWarning
           modalIsOpen={showExitModal}
           onExit={() => {
-            setShowExitModal(false);
-            goToNext();
+            hideExitModal(false);
+            handleNextStep(true);
           }}
           onCancel={() => {
-            setShowExitModal(false);
+            hideExitModal(false);
           }}
         />
       )}
