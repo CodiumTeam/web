deebd8513b062e9c02e9feb7ca19678444ab9c15
Jose Cabrera <jose@codium.team>
2022-06-07 15:16:17 +0200

    add card animation

--- src/js/site.js
+++ src/js/site.js
@@ -3,6 +3,7 @@ import { listenDropdown } from './common/dropdown';
 import './common/contact-form-with-message';
 
 import '../sass/site.scss';
+import { scrollToElement } from './common/scrollToElement';
 
 listenDropdown();
 events.initTrackEvents();
@@ -11,20 +12,25 @@ document.querySelectorAll('[data-type="card-services"]').forEach((card) => {
   const viewMoreBtn = card.querySelector(
     '[data-type="card-services-view-more"]'
   );
-
   const viewLessBtn = card.querySelector(
     '[data-type="card-services-view-less"]'
   );
+  const cardTitle = card.querySelector('[data-type="card-services-title"]');
 
   viewMoreBtn.addEventListener('click', () => {
     card.classList.toggle('card--open');
     viewMoreBtn.classList.add('hidden');
+    viewLessBtn.classList.remove('hidden');
+    scrollToElement(cardTitle);
   });
 
   viewLessBtn.addEventListener('click', () => {
     card.classList.toggle('card--open');
+    viewLessBtn.classList.add('hidden');
+
     setTimeout(() => {
       viewMoreBtn.classList.remove('hidden');
+      scrollToElement(card);
     }, 0);
   });
 });
