43c5ea94a74deee61c8a8a81ab7a08df56820a0e
Jose Cabrera <jose@codium.team>
2022-06-07 10:31:15 +0200

    Add animation to page

--- src/js/site.js
+++ src/js/site.js
@@ -6,3 +6,25 @@ import '../sass/site.scss';
 
 listenDropdown();
 events.initTrackEvents();
+
+document.querySelectorAll('[data-type="card-services"]').forEach((card) => {
+  const viewMoreBtn = card.querySelector(
+    '[data-type="card-services-view-more"]'
+  );
+
+  const viewLessBtn = card.querySelector(
+    '[data-type="card-services-view-less"]'
+  );
+
+  viewMoreBtn.addEventListener('click', () => {
+    card.classList.toggle('card--open');
+    viewMoreBtn.classList.add('hidden');
+  });
+
+  viewLessBtn.addEventListener('click', () => {
+    card.classList.toggle('card--open');
+    setTimeout(() => {
+      viewMoreBtn.classList.remove('hidden');
+    }, 0);
+  });
+});
