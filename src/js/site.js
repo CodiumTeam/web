1ab1dbf1dad027c148daf30de4c2b6378446aa34
Jose Cabrera <jose@codium.team>
2022-06-08 10:56:41 +0200

    Generates random image

--- src/js/site.js
+++ src/js/site.js
@@ -4,40 +4,58 @@ import './common/contact-form-with-message';
 
 import '../sass/site.scss';
 import { scrollToElement } from './common/scrollToElement';
+import { isInView } from 'isinview';
 
 listenDropdown();
 events.initTrackEvents();
 
-document.querySelectorAll('[data-type="card-services"]').forEach((card) => {
-  const viewMoreBtn = card.querySelector(
-    '[data-type="card-services-view-more"]'
-  );
-  const viewLessBtn = card.querySelector(
-    '[data-type="card-services-view-less"]'
-  );
-  const cardTitle = card.querySelector('[data-type="card-services-title"]');
-  const iWantItBtn = card.querySelector('[data-role="button"]');
+document
+  .querySelectorAll('[data-type="card-services"]')
+  .forEach((card, index, arr) => {
+    let randomNumber = Math.floor(Math.random() * arr.length);
+    const collectionID = 856079;
 
-  iWantItBtn.addEventListener('click', (ev) => {
-    const serviceType = ev.target.dataset.type;
-    const $serviceNameInput = document.querySelector('#serviceName');
-    $serviceNameInput.value = serviceType;
-  });
+    isInView(
+      card,
+      () => {
+        fetch(
+          `https://source.unsplash.com/collection/${collectionID}/?sig=${randomNumber}&blur`
+        ).then((response) => {
+          const image = card.querySelector('[data-type="card-services-image"]');
+          image.style.backgroundImage = `url(${response.url})`;
+        });
+      },
+      { once: true }
+    );
 
-  viewMoreBtn.addEventListener('click', () => {
-    card.classList.toggle('card--open');
-    viewMoreBtn.classList.add('hidden');
-    viewLessBtn.classList.remove('hidden');
-    scrollToElement(cardTitle);
-  });
+    const viewMoreBtn = card.querySelector(
+      '[data-type="card-services-view-more"]'
+    );
+    const viewLessBtn = card.querySelector(
+      '[data-type="card-services-view-less"]'
+    );
+    const iWantItBtn = card.querySelector('[data-role="button"]');
 
-  viewLessBtn.addEventListener('click', () => {
-    card.classList.toggle('card--open');
-    viewLessBtn.classList.add('hidden');
+    iWantItBtn.addEventListener('click', (ev) => {
+      const serviceType = ev.target.dataset.type;
+      const $serviceNameInput = document.querySelector('#serviceName');
+      $serviceNameInput.value = serviceType;
+    });
 
-    setTimeout(() => {
-      viewMoreBtn.classList.remove('hidden');
+    viewMoreBtn.addEventListener('click', () => {
+      card.classList.toggle('card--open');
+      viewMoreBtn.classList.add('hidden');
+      viewLessBtn.classList.remove('hidden');
       scrollToElement(card);
-    }, 0);
+    });
+
+    viewLessBtn.addEventListener('click', () => {
+      card.classList.toggle('card--open');
+      viewLessBtn.classList.add('hidden');
+
+      setTimeout(() => {
+        viewMoreBtn.classList.remove('hidden');
+        scrollToElement(card);
+      }, 0);
+    });
   });
-});
