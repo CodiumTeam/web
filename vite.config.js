36a2cf486707d444575ae24892802d51b9860dc9
Luis Rovirosa Mairlot <luis@codium.team>
2022-09-26 16:49:45 +0200

    [refactoring-to-patterns] rename to Spanish

--- vite.config.js
+++ vite.config.js
@@ -18,9 +18,9 @@ const inputs = {
   404: resolve(SRC, '404.html'),
   'tdd-challenge': resolve(SRC, 'tdd-challenge.html'),
   services: resolve(SRC, 'servicios.html'),
-  'curso-refactoring-to-patterns': resolve(
+  'curso-refactoring-a-patrones': resolve(
     SRC,
-    'curso-refactoring-to-patterns.html'
+    'curso-refactoring-a-patrones.html'
   ),
 };
 
