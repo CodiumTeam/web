c76a62df9b2fc73221f8745ff6fa647cf2f8dbde
Jose Cabrera <jose@codium.team>
2023-02-24 10:22:49 +0100

    Remove img event

--- src/js/resources.js
+++ src/js/resources.js
@@ -3,9 +3,3 @@ import { listenDropdown } from './common/dropdown';
 import '../sass/resources.scss';
 
 listenDropdown();
-
-document.querySelectorAll('.js-image').forEach((imageLink) => {
-  const $img = imageLink.firstChild.nextElementSibling;
-
-  imageLink.href = $img.src;
-});
