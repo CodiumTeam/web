51ee3243213c24f29be8c678dd3abe0237098dcc
Jose Cabrera <jose@codium.team>
2023-08-17 12:35:49 +0200

    CY: add patrones + qa tests

---
+++ cypress/e2e/pages/courses/patrones.spec.js
@@ -0,0 +1,57 @@
+/// <reference types="cypress" />
+
+import { isMobile } from '../../isMobile';
+
+const devices = ['desktop', 'iphone-6'];
+
+describe('User navigation Patrones page', () => {
+  devices.forEach((device) => {
+    describe(`From navbar in ${device}`, () => {
+      beforeEach(() => {
+        if (isMobile(device)) {
+          cy.viewport(device);
+        }
+
+        cy.visit('/curso-refactoring-a-patrones.html');
+
+        if (isMobile(device)) {
+          cy.get('#js-menu-button').click();
+        }
+      });
+
+      it('Should navigate to course benefits block in patrones page', () => {
+        cy.get('.navbar__item').should('contain', 'Beneficios');
+        cy.get('.navbar__item').first().click();
+        cy.isInViewport('#benefits');
+      });
+
+      it('Should navigate to the course content block in patrones page', () => {
+        cy.get('.navbar__item').eq(1).should('contain', 'Contenido');
+        cy.get('.navbar__item').eq(1).click();
+        cy.isInViewport('#training-content-timeline');
+      });
+
+      it('Should navigate to the course methodology block in patrones page', () => {
+        cy.get('.navbar__item').eq(2).should('contain', 'Metodología');
+        cy.get('.navbar__item').eq(2).click();
+        cy.isInViewport('#methodology');
+      });
+
+      it('Should navigate to the course faqs block in patrones page', () => {
+        cy.get('.navbar__item').eq(3).should('contain', 'FAQs');
+        cy.get('.navbar__item').eq(3).click();
+        cy.isInViewport('#faqs');
+      });
+
+      it('Should navigate to the course contact block in patrones page', () => {
+        cy.get('.navbar__item').eq(4).should('contain', 'Contactar');
+        cy.get('.navbar__item').eq(4).click();
+        cy.isInViewport('#contact');
+      });
+
+      it('Should be able to see service list in a dropdown', () => {
+        cy.validateServiceListInDropdown();
+      });
+    });
+  });
+});
