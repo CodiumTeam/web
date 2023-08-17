f4bb0a1cc5326d07d5db00e68d0b17550e309bc3
Jose Cabrera <jose@codium.team>
2023-08-17 18:11:20 +0200

    Change 404 page

--- src/js/404.js
+++ src/js/404.js
@@ -3,3 +3,15 @@ import { listenDropdown } from './common/dropdown';
 import '../sass/404.scss';
 
 listenDropdown();
+
+document.querySelectorAll('.icon-test').forEach((item, index) => {
+  const timeInMS = Math.floor(Math.random() * 6);
+  setTimeout(() => {
+    const element = item;
+    if (element.classList.contains('test-error')) {
+      element.classList.add('icon-test--error');
+    } else {
+      element.classList.add('icon-test--success');
+    }
+  }, timeInMS * 1000);
+});
