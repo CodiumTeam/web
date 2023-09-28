059e62282ca777114114ffb38a8509173da153e7
Hugo Chinchilla <hugo@codium.team>
2023-09-28 12:30:45 +0200

    run lint-staged without needing a gitconfig

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,3 +1,11 @@
 #!/bin/node
 
+GIT_NAME="Lint-staged"
+GIT_EMAIL="nobody@codium.team"
+
+export GIT_AUTHOR_NAME=$GIT_NAME
+export GIT_COMMITTER_NAME=$GIT_NAME
+export GIT_AUTHOR_EMAIL=$GIT_EMAIL
+export GIT_COMMITTER_EMAIL=$GIT_EMAIL
+
 npm run lint-staged --no-update-notifier
