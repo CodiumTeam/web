ec76216dd60ad0f479225c9f56b4e1c6682211b6
Jose Cabrera <jose@codium.team>
2024-11-25 16:17:38 +0100

    Rename var

--- build-utils.mjs
+++ build-utils.mjs
@@ -20,12 +20,12 @@ export const i18n = new I18n({
 
 
 export const compileHtml = (html, ejsData = {}) => {
-  let newHTML = html.replace(
+  const compiledHtml = html.replace(
     /<t\b[^>]*>(.*?)<\/t>/gs, // Match <Trans>...</Trans> tags
     (match, p1) => `<%= __('${p1.trim()}') %>`
   );
 
-  return ejs.compile(newHTML, {
+  return ejs.compile(compiledHtml, {
     views: [path.join(__dirname, 'src')],
     async: false,
   })({
