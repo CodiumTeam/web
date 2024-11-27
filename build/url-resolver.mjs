21c934dfa00021b2c121c729947bcb0817c4fd50
Jose Cabrera <jose@codium.team>
2024-11-27 15:12:14 +0100

    Translate TDD

--- build/url-resolver.mjs
+++ build/url-resolver.mjs
@@ -1,7 +1,15 @@
-import {DEFAULT_LANG} from "./i18n-utils.mjs";
+import { DEFAULT_LANG } from './i18n-utils.mjs';
 
 export function urlResolver(path, locale = process.env.locale) {
-  const host = process.env.BASE_DOMAIN || "https://www.codium.team";
+  const host = process.env.BASE_DOMAIN || 'https://www.codium.team';
+
+  if (path.startsWith('http')) {
+    if (locale !== DEFAULT_LANG) {
+      return path + '/' + locale;
+    }
+
+    return path;
+  }
 
   if (locale && locale !== DEFAULT_LANG) {
     return `${host}/${locale}${path}`;
