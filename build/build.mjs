9c09dc3d29ba7d74d782ca87f1926224de45623f
Jose Cabrera <jose@codium.team>
2024-11-28 17:33:43 +0100

    Rename url based on language just for english

--- build/build.mjs
+++ build/build.mjs
@@ -1,5 +1,12 @@
 import { execSync } from 'node:child_process';
-import { i18n } from './i18n-utils.mjs';
+import {DEFAULT_LANG, i18n} from './i18n-utils.mjs';
+import {keyTranslations} from "./url-resolver.mjs";
+import path from "node:path";
+import fs from "node:fs";
+
+import {fileURLToPath} from "node:url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 function runBuild(locale) {
   console.log('Building for location: ' + locale)
@@ -8,8 +15,24 @@ function runBuild(locale) {
   execSync('vite build --mode=production', { stdio: 'inherit' });
 }
 
-const locales = i18n.getLocales();
 
+function renameFilesToMatchUrls(directory) {
+  for (const [originalName, translatedName] of Object.entries(keyTranslations)) {
+    const originalPath = path.join(directory, originalName.replace('/', ''));
+    const translatedPath = path.join(directory, translatedName.replace('/', ''));
+
+    if (fs.existsSync(originalPath)) {
+        fs.renameSync(originalPath, translatedPath);
+    } else {
+      throw Error(`File not found: ${originalName}`)
+    }
+  }
+}
+
+const locales = i18n.getLocales();
 for (const locale of locales) {
   runBuild(locale);
+  if (locale !== DEFAULT_LANG) {
+    renameFilesToMatchUrls(path.resolve(__dirname, '..', 'dist', locale));
+  }
 }
--- build/url-resolver.mjs
+++ build/url-resolver.mjs
@@ -1,4 +1,13 @@
 import { DEFAULT_LANG } from './i18n-utils.mjs';
+export const keyTranslations = {
+  '/curso-docker.html': '/docker-training.html',
+  '/curso-legacy-code.html': '/legacy-code-training.html',
+  '/curso-quality-assurance.html': '/quality-assurance-training.html',
+  '/curso-refactoring-a-patrones.html': '/refactoring-patterns-training.html',
+  '/curso-tdd.html': '/tdd-training.html',
+  '/desarrollo.html': '/development.html',
+  '/programa-de-aceleracion.html': '/acceleration-program.html',
+};
 
 export function urlResolver(path, locale = process.env.locale) {
   const host = process.env.BASE_DOMAIN || 'https://www.codium.team';
@@ -8,12 +17,18 @@ export function urlResolver(path, locale = process.env.locale) {
       return path + '/' + locale;
     }
 
-    return path;
+    return translateUrlInProduction(path);
   }
 
   if (locale && locale !== DEFAULT_LANG) {
-    return `${host}/${locale}${path}`;
+    return `${host}/${locale}${translateUrlInProduction(path)}`;
   }
 
   return host + path;
+}
+
+function translateUrlInProduction(path) {
+  if (process.env.NODE_ENV !== 'production') return path;
+
+  return keyTranslations[path] || path; // Return the translated path or the original if not found
 }
